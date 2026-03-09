e47962bf046206a94b7b65979359367c60308987
sh0k0ma <shohei.komatsu.0212@gmail.com>
2026-03-09 16:10:57 +0900

    Refactor code structure for improved readability and maintainability

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client";
+import ThinkingFramework from "./thinking-framework-v2";
+
+createRoot(document.getElementById("root")).render(<ThinkingFramework />);
